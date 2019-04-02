58124063038950e89038021c2811c251c2f20138
ismetbingol <37374074+ismetbingol@users.noreply.github.com>
2019-04-03 02:14:14 +0300

    Taking input and output file from command line is implemented

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        JsonIO jsonIO = new JsonIO("input.json", "output.json");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter intput json: ");
+        String inputJson = scanner.next();
+        System.out.print("Enter output json: ");
+        String outputJson = scanner.next();
+        JsonIO jsonIO = new JsonIO(inputJson, outputJson);
         InputModel inputModel = jsonIO.read();
         runTournaments(inputModel.getPlayers(), inputModel.getTournaments());
         Collections.sort(inputModel.getPlayers());
